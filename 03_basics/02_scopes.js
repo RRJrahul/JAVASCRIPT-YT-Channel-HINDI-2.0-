4ad42193157b37bc509d1c6524a5bd8daf4f60b7
Rahul Ranjan <127117589+RRJrahul@users.noreply.github.com>
2024-09-23 06:04:15 +0000

    This is all about scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,15 +3,55 @@ let a = 300
 if(true){
 let a = 10    //they all have limitation within this block
 const b = 20
-console.log("INNER:",a);
+//console.log("INNER:",a);
 
 }
 for(let i=0;i < Array.length;i++){
-    const element = array[i];
+    const element = Array[i];
     
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 // console.log(c);
+
+//PART 2
+function one(){
+    const username = "Rahul"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true){
+    const username = "rrj"
+    if(username === "rrj"){
+        const website = "Xbeats"
+        console.log(username + website);
+        }
+    //console.log(website);
+    }
+    //console.log(username);
+
+    //++++++++++++++++++++ Intresting ++++++++++++
+
+    console.log(addone(5));
+    function addone(num){
+        return num + 1
+    }
+   
+
+    //console.log(addTwo(5));
+    const addTwo = function(num){//here we declared a function abd we hold it in  a variable
+        return num + 2
+    }
+    
+    
+
+
+
