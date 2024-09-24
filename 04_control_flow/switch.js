2927c5b93220043bc2d566a89f5db70e7c52f020
Rahul Ranjan <127117589+RRJrahul@users.noreply.github.com>
2024-09-24 15:41:26 +0000

    This was all about switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "Feb"                //const month = 3 
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        break;
+}
