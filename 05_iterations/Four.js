930a9bc9a9a834974df90715b55bac908ae289ea
Rahul Ranjan <127117589+RRJrahul@users.noreply.github.com>
2024-09-25 19:34:46 +0000

    For in loop

---
+++ 05_iterations/Four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift:"swift by apple"
+}
+// it's for-in loop
+for (const key in myObject) {
+    //console.log(key);
+    //console.log(myObject[key]);
+    //console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+// const programming = ["js","rb","py","java","cpp"]
+// for (const key in programming) {
+//     console.log(key);
+// }
+
+//Maps 
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United state of america")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// for (const key in map) {
+//     console.log(key);
+//     }
+
